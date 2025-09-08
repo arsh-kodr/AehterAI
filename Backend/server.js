8d1a839dd76926b23c2814c5d194ce2746c9c19f
arsh-kodr <raisahab2364@gmail.com>
2025-09-08 06:11:27 +0530

    Add message model and AI service; enhance socket server for message handling and authentication

--- Backend/server.js
+++ Backend/server.js
@@ -3,12 +3,17 @@ require("dotenv").config();
 const app = require("./src/app");
 const connectToDB = require("./src/db/db");
 const initSocketServer = require("./src/sockets/socket.server");
-const httpServer  = require("http").createServer(app);
+const httpServer = require("http").createServer(app);
 
 connectToDB()
-initSocketServer(httpServer);
+  .then(() => {
+    initSocketServer(httpServer);
 
-httpServer.listen(3000 , () => {
-  console.log("Server is running on PORT 3000");
-  
-})
+    httpServer.listen(3000, () => {
+      console.log("Server is running on PORT 3000");
+    });
+  })
+  .catch((err) => {
+    console.log("Error in connecting to DB : ", err);
+    process.exit(1);
+  });
--- Backend/src/sockets/socket.server.js
+++ Backend/src/sockets/socket.server.js
@@ -1,12 +1,73 @@
 const { Server } = require("socket.io");
+const cookie = require("cookie");
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/user.model");
+const { generateResponse } = require("../service/ai.service");
+const messageModel = require("../models/message.model");
 
 function initSocketServer(httpServer) {
-    const io = new Server(httpServer);
+  const io = new Server(httpServer);
 
-    io.on("connection" , (socket) => {
-        console.log("New Socket Connection : " , socket.id);
-        
-    })
+  io.use(async (socket, next) => {
+    const cookies = cookie.parse(socket.handshake.headers?.cookie || "");
+
+    if (!cookies.token) {
+      return next(new Error("Authentication Error :  No token provided"));
+    }
+
+    try {
+      const decoded = jwt.verify(cookies.token, process.env.JWT_SECRET);
+
+      const user = await userModel.findById(decoded.id);
+
+      socket.user = user;
+
+      next();
+    } catch (error) {
+      next(new Error("Authentication Error :  Invalid Token"));
+      return error;
+    }
+  });
+
+  io.on("connection", (socket) => {
+    console.log("New Socket Connection : ", socket.id);
+
+    socket.on("ai-message", async (messagePayload) => {
+      console.log("Message Payload from client : ", messagePayload);
+
+      await messageModel.create({
+        chat: messagePayload.chat,
+        user: socket.user._id,
+        content: messagePayload.content,
+        role: "user",
+      });
+
+      const chatHistory = (await messageModel.find({
+        chat: messagePayload.chat,
+      }).sort({ createdAt: 1 }).limit(20).lean()).reverse();
+
+      const response = await generateResponse(
+        chatHistory.map((item) => {
+          return {
+            role: item.role,
+            parts: [{ text: item.content }],
+          };
+        })
+      );
+
+      await messageModel.create({
+        chat: messagePayload.chat,
+        user: socket.user._id,
+        content: response,
+        role: "model",
+      });
+
+      socket.emit("ai-response", {
+        content: response,
+        chat: messagePayload.chat,
+      });
+    });
+  });
 }
 
 module.exports = initSocketServer;
